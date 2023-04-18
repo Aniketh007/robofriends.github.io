d01c4d1aa6b11333d925897275134db5bff4456e
Aniketh007 <96186902+Aniketh007@users.noreply.github.com>
2023-04-18 23:11:05 +0530

    Second Website files

---
+++ robots.js
@@ -0,0 +1,44 @@
+export const robots= [
+	{
+		id: 1,
+		name:'leana stark',
+		username:'stark',
+		email:'leanastark@gmail.com'
+	},
+	{
+		id:2,
+		name:'robert barothean',
+		username:'baratheon',
+		email:'robertB@gmail.com'
+	},
+	{
+		id:3,
+		name:'jon snow',
+		username:'bastard',
+		email:'jonsnow@gmail.com'
+	},
+	{
+		id:4,
+		name:'jammie lannister',
+		username:'lannister',
+		email:'jammielannister@gmail.com'
+	},
+	{
+		id:5,
+		name:'danerys targeriyan',
+		username:'danny',
+		email:'daneryst@gmail.com'
+	},
+	{
+		id:7,
+		name:'bayllish',
+		username:'littel finger',
+		email:'ass@gmail.com'
+	},
+	{
+		id:8,
+		name:'Arya stark',
+		username:'arya',
+		email:'aryas@gmai.com'
+	}
+]
---
+++ searchBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const searchBox=({searchfield,searchChange})=> {
+	return (
+		<div className='pa2'>
+			<input 
+			className='bg-light-blue dib br3 pa3 ma2 '		
+			type='search' 
+			placeholder='search robots'
+			onChange={searchChange}
+			/>
+		</div>
+	)
+}
+
+export default searchBox;
